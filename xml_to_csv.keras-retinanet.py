8dec66795c5c14ba6eb5bcc07f83e80e44cc3ac8
Vinoth Pandian <vinothpandian@live.com>
2018-10-17 20:18:59 +0200

    keras retinanet trainable

---
+++ xml_to_csv.keras-retinanet.py
@@ -0,0 +1,32 @@
+import os
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (os.path.join(os.getcwd(), "images", root.find('filename').text),
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text),
+                     member[0].text
+                     )
+            xml_list.append(value)
+    xml_df = pd.DataFrame(xml_list, columns=None)
+    return xml_df
+
+
+def main():
+    image_path = os.path.join(os.getcwd(), 'annotations')
+    xml_df = xml_to_csv(image_path)
+    xml_df.to_csv('annotations.csv', index=None)
+    print('Successfully converted xml to csv.')
+
+
+main()
